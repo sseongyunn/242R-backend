5b6a3df0a7c70c6c16a5f8785641126abb12a726
sseongyunn <sseongyunn@korea.ac.kr>
2024-11-06 16:39:06 +0900

    디렉토리 정리

--- week2/import_custom_module.js
+++
@@ -1,9 +0,0 @@
-const circularShapes = require('./circular-shapes');
-
-const r = 10;
-const h = 20;
-
-console.log(`Circumference = ${circularShapes.getCircumference(r)}`);
-console.log(`Circle Area = ${circularShapes.getCircleArea(r)}`);
-console.log(`Sphere Volume = ${circularShapes.getSphereVolume(r)}`);
-console.log(`Cylinder Surface Area = ${circularShapes.getCylinderSurfaceArea(r, h)}`);
